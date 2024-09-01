2cf81556ab5ac402cb3eadb147b88342998ff3ca
MistyIslam5 <mdmisty5@gmail.com>
2024-09-01 20:46:10 +0600

    javascript loop

---
+++ for-of.js
@@ -0,0 +1,16 @@
+const numbers = [67, 78, 87, 98, 99, 43, 232, 34,];
+
+console.log(numbers);
+
+for (const number of numbers) {
+
+    console.log(number);
+    
+
+}
+
+const fruits = ['banana', 'orange', 'apple', 'jambura', 'tomato'];
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
