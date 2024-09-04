71fc37ed9e152469630f33b5c757b405211bc263
MistyIslam5 <mdmisty5@gmail.com>
2024-09-04 11:29:26 +0600

    while loop 3and 4 task complet

---
+++ whiletask3.js
@@ -0,0 +1,36 @@
+/**
+ * sub task 1 
+ * 81 theke 131 porjonto somosto bizur number er jugfol ber kora
+ *
+ */
+
+let i= 81;
+let sum = 0;
+while (i<=131){
+    if (i % 2 !== 0){
+        console.log(i)
+        sum = sum + i;
+    }
+    i++;
+}
+console.log(sum);
+
+
+
+
+
+/**
+ * sub task 2 
+ * 206 theke 331 porjonto somosto zur number er jugfol ber kora
+ */
+
+let j = 206;
+let total = 0;
+while (j<=311){
+    if (j % 2 === 0){
+        total = total +j;
+    }
+    j++;
+}
+
+console.log(total);
---
+++ whiletask4.js
@@ -0,0 +1,12 @@
+
+/**
+ * As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number
+ */
+
+
+let i = 5;
+let j = 0;
+while (j <= 10){
+    console.log(i + '*' + j + "=" + i*j);
+    j++;
+}
